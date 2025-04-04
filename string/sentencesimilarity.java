ab0101daca3d3c2c6f097f09b1e4ad9ac727fc3e
VSD10 <vsdh2004@gmail.com>
2025-04-05 04:37:22 +0800

    sentencesimilarity

---
+++ string/sentencesimilarity.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean areSentencesSimilar(String sentence1, String sentence2) {
+        String[] s1=sentence1.split(" ");
+        String[] s2=sentence2.split(" ");
+        if(s1.length<s2.length)
+        {
+            String[] t=s1;
+            s1=s2;
+            s2=t;
+        }
+        int start=0;
+        int end=0;
+        while(start<s2.length && s1[start].equals(s2[start]))
+        {
+            start++;
+        }
+        while(end<s2.length && s1[s1.length-end-1].equals(s2[s2.length-end-1]))
+        {
+            end++;
+        }
+      
+        return (start+end>=s2.length);
+
+        
+    }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String sentence1 = "My name is John";
+        String sentence2 = "My name is";
+        boolean result = solution.areSentencesSimilar(sentence1, sentence2);
+        System.out.println("Are sentences similar? " + result); // Expected output: true
+    }
+}
