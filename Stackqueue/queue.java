293571a4897b11c8c7d654cafe0c1c88345173fd
VSD10 <vsdh2004@gmail.com>
2025-06-30 16:30:01 +0530

    added

--- Stackqueue/queue.java
+++ Stackqueue/queue.java
@@ -1,10 +1,40 @@
 public class queue{
     public static void main(String[]args)
     {
-        q ss=new q ss();
+        ss q=new ss();
+        q.push(0);
+        q.push(1);
+        q.push(2);
+        q.pop();
+        q.peek();
+        q.size();
 
     }
 }
-class s{
-    public static void main
+class ss{
+    int s=1000;
+    int arr[]=new int[s];
+    int start=0,end=-1;
+    void push(int x)
+    {
+        end++;
+        arr[end]=x;
+    }
+    void peek()
+    {
+        System.out.println(arr[start]);
+
+    }
+    void pop()
+    {
+        int pop=arr[start];
+        start++;
+        System.out.println("pop"+ pop);
+
+    }
+    void size()
+    {
+        System.out.println(end+1);
+    }
+
 }
